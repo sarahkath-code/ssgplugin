997d899ed9b07ada0e9ddf06ec54b7bb40c9c930
sarahkath-code <70974517+sarahkath-code@users.noreply.github.com>
2021-10-15 10:44:14 +0200

    template plugin
    template plugin

---
+++ ssgplugin/ssgplugin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Plugin name: SSG plugin
+* Description: A plugin for testing how to make plugins.
+*/
+
+function beskedfunktion(){
+    $mycode = '<h2>Countdown to social event!</h2>
+    <section id="mysection">
+    <section class="festival_teaser_sec">
+
+    <div class="countdown_sec">
+      <h1>UNDER GADEN</h1>
+      <h2>- FESTIVALLEN STARTER OM</h2>
+      <div class="countdown_container">
+          <div class="countdown">
+              <div id="day">Dage</div>
+              <div id="hour">Timer</div>
+
+          </div>
+      </div>
+    </section>
+    ';  
+    return $mycode;
+}
+
+function register_styles_plugin (){
+    wp_enqueue_style('CustomStylesheet', plugins_url('ssgplugin/css/style.css'));
+    wp_enqueue_script('CustomScript', plugins_url('ssgplugin/js/script.js'),array(),null,true);
+    }
+
+    add_action('wp_enqueue_scripts', 'register_styles_plugin');
+    
+    add_shortcode('ssgplugin','beskedfunktion');
+
+?>
